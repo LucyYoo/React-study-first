360070137cad7ce1caf13ee1455478a811476d32
zero <yj0zero@gmail.com>
2022-10-16 19:48:47 +0900

    week2/section12/TemparatureCalculator

---
+++ src/week2/Temperature/TemperatureInput.jsx
@@ -0,0 +1,19 @@
+const scaleNames = {
+    C: "섭씨",
+    f: "화씨",
+};
+
+export default function TemparatureInput(props) {
+    const handleChange = (event) => {
+        props.onTemperatureChange(event.target.value);
+    };
+
+  return (
+    <fieldset>
+        <legend>
+            온도를 입력해주세요(단위: {scaleNames[props.scale]});
+        </legend>
+        <input value={props.temperature} onChange={handleChange} />
+    </fieldset>
+  );
+}
